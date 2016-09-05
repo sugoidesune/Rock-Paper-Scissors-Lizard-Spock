4bad221cf36712376882d3b4dd37d1b0e46d8278
Timar abi <Timar abi>
2016-09-05 23:11:21 +0200

    wifi keyword

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def post_webhook():
 
                     if 'text' in messaging_event['message']:
                         # basic level
-                        #if message_text == "wifi":
+                        if message_text == "wifi":
                         # Fetch the data from Wiener Linien's API
                         reply_with_text(sender_id, "hi how are you doing?")
                         result = get_url("http://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&srsName=EPSG:4326&outputFormat=json&typeName=ogdwien:WLANWRLOGD")
